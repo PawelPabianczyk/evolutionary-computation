9afb1455d71fdbee1bb275547ee8d4ff0b24f4d9
MuszKarol <karol.musz99@gmail.com>
2022-10-02 13:14:05 +0200

    Create SpringBoot-based project

---
+++ src/main/java/pl/pk/evolutionarycomputation/EvolutionaryComputationApplication.java
@@ -0,0 +1,13 @@
+package pl.pk.evolutionarycomputation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvolutionaryComputationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvolutionaryComputationApplication.class, args);
+	}
+
+}
